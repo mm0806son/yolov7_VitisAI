9d27864e3028e8f983e2abfeee32459967a87033
Zijie NING <nzjbb@163.com>
2021-11-19 17:29:00 -0800

    v3.0

--- MNIST_execute.py
+++ MNIST_execute.py
@@ -1,3 +1,16 @@
+'''
+Copyright 2020 Xilinx Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
 from ctypes import *
 from typing import List
 import cv2
@@ -11,18 +24,21 @@ import time
 import sys
 import argparse
 
-divider='---------------------------'
+_divider = '-------------------------------'
 
-def preprocess_fn(image_path):
+
+def preprocess_fn(image_path, fix_scale):
     '''
     Image pre-processing.
-    Opens image as grayscale then normalizes to range 0:1
+    Opens image as grayscale, adds channel dimension, normalizes to range 0:1
+    and then scales by input quantization scaling factor
     input arg: path of image file
     return: numpy array
     '''
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     image = image.reshape(28,28,1)
-    image = image/255.0
+    image = image * (1/255.0) * fix_scale
+    image = image.astype(np.int8)
     return image
 
 
@@ -42,17 +58,25 @@ def get_child_subgraph_dpu(graph: "Graph") -> List["Subgraph"]:
 
 
 def runDPU(id,start,dpu,img):
-
     '''get tensor'''
     inputTensors = dpu.get_input_tensors()
     outputTensors = dpu.get_output_tensors()
     input_ndim = tuple(inputTensors[0].dims)
     output_ndim = tuple(outputTensors[0].dims)
 
+    # we can avoid output scaling if use argmax instead of softmax
+    #output_fixpos = outputTensors[0].get_attr("fix_point")
+    #output_scale = 1 / (2**output_fixpos)
+
     batchSize = input_ndim[0]
     n_of_images = len(img)
     count = 0
     write_index = start
+    ids=[]
+    ids_max = 10
+    outputData = []
+    for i in range(ids_max):
+        outputData.append([np.empty(output_ndim, dtype=np.int8, order="C")])
     while count < n_of_images:
         if (count+batchSize<=n_of_images):
             runSize = batchSize
@@ -60,25 +84,30 @@ def runDPU(id,start,dpu,img):
             runSize=n_of_images-count
 
         '''prepare batch input/output '''
-        outputData = []
         inputData = []
-        inputData = [np.empty(input_ndim, dtype=np.float32, order="C")]
-        outputData = [np.empty(output_ndim, dtype=np.float32, order="C")]
+        inputData = [np.empty(input_ndim, dtype=np.int8, order="C")]
 
         '''init input image to input buffer '''
         for j in range(runSize):
             imageRun = inputData[0]
             imageRun[j, ...] = img[(count + j) % n_of_images].reshape(input_ndim[1:])
-
         '''run with batch '''
-        job_id = dpu.execute_async(inputData,outputData)
-        dpu.wait(job_id)
-
-        '''store output vectors '''
-        for j in range(runSize):
-            out_q[write_index] = np.argmax(outputData[0][j])
-            write_index += 1
-        count = count + runSize
+        job_id = dpu.execute_async(inputData,outputData[len(ids)])
+        ids.append((job_id,runSize,start+count))
+        count = count + runSize 
+        if count<n_of_images:
+            if len(ids) < ids_max-1:
+                continue
+        for index in range(len(ids)):
+            dpu.wait(ids[index][0])
+            write_index = ids[index][2]
+            '''store output vectors '''
+            for j in range(ids[index][1]):
+                # we can avoid output scaling if use argmax instead of softmax
+                # out_q[write_index] = np.argmax(outputData[0][j] * output_scale)
+                out_q[write_index] = np.argmax(outputData[index][0][j])
+                write_index += 1
+        ids=[]
 
 
 def app(image_dir,threads,model):
@@ -95,14 +124,20 @@ def app(image_dir,threads,model):
     for i in range(threads):
         all_dpu_runners.append(vart.Runner.create_runner(subgraphs[0], "run"))
 
+    # input scaling
+    input_fixpos = all_dpu_runners[0].get_input_tensors()[0].get_attr("fix_point")
+    input_scale = 2**input_fixpos
+
     ''' preprocess images '''
+    print (_divider)
     print('Pre-processing',runTotal,'images...')
     img = []
     for i in range(runTotal):
         path = os.path.join(image_dir,listimage[i])
-        img.append(preprocess_fn(path))
+        img.append(preprocess_fn(path, input_scale))
 
     '''run threads '''
+    print (_divider)
     print('Starting',threads,'threads...')
     threadAll = []
     start=0
@@ -125,7 +160,7 @@ def app(image_dir,threads,model):
     timetotal = time2 - time1
 
     fps = float(runTotal / timetotal)
-    print(divider)
+    print (_divider)
     print("Throughput=%.2f fps, total frames = %.0f, time=%.4f seconds" %(fps, runTotal, timetotal))
 
 
@@ -142,10 +177,8 @@ def app(image_dir,threads,model):
             wrong += 1
     accuracy = correct/len(out_q)
     print('Correct:%d, Wrong:%d, Accuracy:%.4f' %(correct,wrong,accuracy))
-    print(divider)
-
+    print (_divider)
     return
-
 
 
 
@@ -157,14 +190,12 @@ def main():
   ap.add_argument('-d', '--image_dir', type=str, default='images', help='Path to folder of images. Default is images')  
   ap.add_argument('-t', '--threads',   type=int, default=1,        help='Number of threads. Default is 1')
   ap.add_argument('-m', '--model',     type=str, default='CNN_zcu102.xmodel', help='Path of xmodel. Default is CNN_zcu102.xmodel')
-
   args = ap.parse_args()  
-  print(divider)
+  
   print ('Command line options:')
   print (' --image_dir : ', args.image_dir)
   print (' --threads   : ', args.threads)
   print (' --model     : ', args.model)
-  print(divider)
 
   app(args.image_dir,args.threads,args.model)
 
