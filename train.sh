1bda070d1e02e5130625656fe68d1309db4fded0
Zijie NING <zijie.ning@univ-ubs.fr>
2023-03-07 17:38:30 +0100

    update script

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 # train models
-python train.py --workers 8 --device 0 --batch-size 32 --data data/odessa.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_odessa.yaml --weights '' --name yolov7_tiny_odessa --hyp data/hyp.scratch.tiny_odessa.yaml
+python train.py --workers 8 --device cpu --batch-size 32 --data data/odessa.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_odessa.yaml --weights '' --name yolov7_tiny_odessa --hyp data/hyp.scratch.tiny_odessa.yaml --epochs 10
