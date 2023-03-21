80750d8ef53a3bf48f6104b6135203e014367fd4
Zijie NING <zijie.ning@univ-ubs.fr>
2023-03-21 17:03:03 +0100

    add resume

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 # train models
-python train.py --workers 20 --device 0 --batch-size 64 --data data/odessa.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_odessa.yaml --weights '' --name yolov7_tiny_odessa --hyp data/hyp.scratch.tiny_odessa.yaml --epochs 300 --project Yolov7_VitisAI
+python train.py --workers 20 --device 0 --batch-size 64 --data data/odessa.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_odessa.yaml --weights '' --name yolov7_tiny_odessa --hyp data/hyp.scratch.tiny_odessa.yaml --epochs 300 --project Yolov7_VitisAI --resume
