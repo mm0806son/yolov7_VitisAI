e1940085344a3e92b4d8f487b1b180d36b4a5047
Zijie NING <zijie.ning@univ-ubs.fr>
2023-03-21 15:39:42 +0100

    update script

--- test_quant.sh
+++ test_quant.sh
@@ -1,8 +1,8 @@
 # quantize model using test.py
 # inspect
-python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 32 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/nodetect.pt --name yolov7_tiny_odessa --inspect --quant_mode float --output_dir quantize_result/nodetect
+python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 32 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/yolov7_tiny_odessa.pt --name yolov7_tiny_odessa --inspect --quant_mode float --output_dir quantize_result/yolov7_tiny_odessa
 # calib
-python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/nodetect.pt --name yolov7_tiny_odessa --quant_mode calib  --output_dir quantize_result/nodetect
+python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/yolov7_tiny_odessa.pt --name yolov7_tiny_odessa --quant_mode calib  --output_dir quantize_result/yolov7_tiny_odessa
 # test
-python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/nodetect.pt --name yolov7_tiny_odessa --quant_mode test --output_dir quantize_result/nodetect
-python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/nodetect.pt --name yolov7_tiny_odessa --quant_mode test --deploy --output_dir quantize_result/nodetect
+python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/yolov7_tiny_odessa.pt --name yolov7_tiny_odessa --quant_mode test --output_dir quantize_result/yolov7_tiny_odessa
+python test_quant.py --data data/odessa_docker.yaml --img 640 --batch-size 1 --conf-thres 0.001 --iou 0.65 --device cpu --weights runs/yolov7_tiny_odessa.pt --name yolov7_tiny_odessa --quant_mode test --deploy --output_dir quantize_result/yolov7_tiny_odessa
