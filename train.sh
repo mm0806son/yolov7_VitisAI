9bcf68646f479d495878959cc096c926f68ab4f5
Zijie NING <73909893+mm0806son@users.noreply.github.com>
2023-03-31 10:05:55 +0200

    Delete train.sh

--- train.sh
+++
@@ -1,4 +0,0 @@
-# train models yolov7_tiny
-python train.py --workers 8 --device cpu --batch-size 32 --data data/odessa_docker.yaml --img 640 640 --cfg cfg/training/yolov7-tiny_odessa.yaml --weights '' --name yolov7_tiny_odessa_nodetect --hyp data/hyp.scratch.tiny_odessa.yaml --epochs 10
-# train models yolo
-# python train.py --workers 8 --device 0 --batch-size 16 --data data/odessa.yaml --img 640 640 --cfg cfg/training/yolov7_odessa.yaml --weights '' --name yolov7_odessa --hyp data/hyp.scratch.custom.yaml
