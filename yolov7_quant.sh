a6512008f429a179d9dc976358827dd2b0f07b71
Zijie NING <73909893+mm0806son@users.noreply.github.com>
2023-03-31 10:06:51 +0200

    Delete yolov7_quant.sh

--- yolov7_quant.sh
+++
@@ -1,9 +0,0 @@
-# inspect model yolov7_tiny
-# python torchhub_quant.py --quant_mode float --inspect --model runs/train/detect/weights/best.pt
-
-# quantifize model yolov7_tiny
-python yolov7_quant.py --quant_mode calib --model runs/train/detect/weights/best.pt
-
-# deploy model yolov7_tiny
-python yolov7_quant.py --quant_mode test --model runs/train/detect/weights/best.pt
-python yolov7_quant.py --quant_mode test --model runs/train/detect/weights/best.pt --subset_len 1 --batch_size=1 --deploy
